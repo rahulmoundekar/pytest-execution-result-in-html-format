0f08f93ea502476f6125c6d583974eeb4cb8fb74
rahul <rahulmoundekar44@gmail.com>
2020-05-19 10:40:50 +0530

    pytest-execution-result-in-html-format

---
+++ login_test/test_Login_Testcase.py
@@ -0,0 +1,17 @@
+import pytest
+
+
+@pytest.fixture()
+def setUp():
+    print("First your want to navigate login page")
+    yield
+    print("once after close all resources")
+
+
+def test_login_by_gmail(setUp):
+    print('Login using gmail account')
+
+
+def test_login_by_official_mail(setUp):
+    print('Login using official mail id')
+    assert 'admin' == 'customer'
